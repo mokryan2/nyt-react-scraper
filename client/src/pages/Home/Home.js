b9dc04041a7f12772fb96d0bf8a512c8c36c858d
Cloud Strife <mokryan17@gmail.com>
2018-10-22 23:34:15 -0500

    Moved components, pages, and utils from original folder

---
+++ client/src/pages/Home/Home.js
@@ -0,0 +1,4 @@
+import React, { Component } from "react";
+import { Link } from "react-router-dom";
+import { Col, Row, Container} from "../../components/Grid";
+import Jumbotron from "../../components/Jumbotron";
